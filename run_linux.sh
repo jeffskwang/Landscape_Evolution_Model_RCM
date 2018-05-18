42b2e32b5dd6bab89898bc77227dba6664bd3548
jeffskwang <jeffskwang@gmail.com>
2018-05-18 00:08:48 -0500

    update

--- run_linux.sh
+++ run_linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-pypy main.py example
-python plot_output.py example
+pypy main.py run_1
+python plot_output.py run_1
+pypy main.py run_2
+python plot_output.py run_2
+pypy main.py run_3
+python plot_output.py run_3
