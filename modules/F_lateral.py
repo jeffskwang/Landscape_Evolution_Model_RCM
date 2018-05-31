b797d0eb255d408c7652b3e30a532cec28528e05
jeffskwang <jeffskwang@gmail.com>
2018-05-31 11:08:12 -0500

    save example run

--- modules/F_lateral.py
+++ modules/F_lateral.py
@@ -93,6 +93,7 @@ def f_lateral(discharge,lateral_incision,area,slope,direction,lateral_incision_t
                                                 bingo = 1
                                         else:
                                                 inverse_radius_curavture = lateral_nodes[curve][2]
-                                                lateral_incision[xlat][ylat] += dt * Kl *(discharge[x2][y2]**m_l)*(slope[x2][y2]**n_l) * inverse_radius_curavture * (discharge_constant * discharge[x2][y2] ** discharge_exponent * dx)
+                                                #lateral_incision[xlat][ylat] += dt * Kl *(discharge[x2][y2]**m_l)*(slope[x2][y2]**n_l) * inverse_radius_curavture * (discharge_constant * discharge[x2][y2] ** discharge_exponent * dx)
+                                                lateral_incision[xlat][ylat] += dt * Kl *(discharge[x1][y1]**m_l)*(slope[x1][y1]**n_l) * inverse_radius_curavture * (discharge_constant * discharge[x1][y1] ** discharge_exponent * dx)
                         
 	return lateral_incision, lateral_incision_threshold
